16136c362452344c74126143d5510ae607f9c719
Venkatesh <venkateshparuchuri20@gmail.com>
2026-03-10 13:54:22 +0530

    miles are calculated

---
+++ level1/problem8.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class problem8{
+    public static void main(String[] args) {
+        
+        double km;
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter distance in kilometers: ");
+        km = input.nextDouble();
+        
+        // 1 mile = 1.6 km
+        double miles = km / 1.6;
+        
+        System.out.println("The total miles is " + miles + 
+                           " mile for the given " + km + " km");
+        
+        input.close();
+    }
+}
