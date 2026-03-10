c1d391e411b775379204f7fa1af715344f9a4f2d
Venkatesh <venkateshparuchuri20@gmail.com>
2026-03-10 13:49:15 +0530

    remaining pens are calculated

---
+++ level1/problem5.java
@@ -0,0 +1,13 @@
+public class problem5 {
+    public static void main(String[] args) {
+        
+        int totalPens = 14;
+        int students = 3;
+        
+        int pensPerStudent = totalPens / students;   // Division
+        int remainingPens = totalPens % students;    // Modulus
+        
+        System.out.println("The Pen Per Student is " + pensPerStudent +
+                           " and the remaining pen not distributed is " + remainingPens);
+    }
+}
